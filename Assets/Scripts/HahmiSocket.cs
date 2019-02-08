db790546efc50a8c535fd970bb6e95f8f8d18bc5
Stanislav Kusovsky <kusovsky95@gmail.com>
2019-02-09 00:44:35 +0200

    init

---
+++ Assets/Scripts/HahmiSocket.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+using WebSocketSharp;
+
+public class HahmiSocket : MonoBehaviour
+{
+    [SerializeField]
+    private string urlServer = "localhost";
+
+    public bool IsConnected {
+        get {
+            return isConnected;
+        }
+        set {
+            if (isConnected == value)
+            {
+                return;
+            }
+            isConnected = value;
+        }
+    }
+
+    [SerializeField]
+    private int portServer = 4444;
+
+    [SerializeField]
+    private float periodTryingConnect = 2.0f; 
+
+    private const string wsFormat = "ws://{0}:{1}";
+
+    private string wsUri = "";
+
+    private WebSocket webSocket = null;
+
+    private bool isConnected = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        wsUri = string.Format(wsFormat, urlServer, portServer);
+        webSocket = new WebSocket(string.Format(wsFormat, urlServer, portServer));
+
+        Debug.Log(string.Format("Hahmi socket awake: {0}:{1}", urlServer, portServer));
+
+        StartCoroutine("TryConnect");
+    }
+
+    IEnumerator TryConnect()
+    {
+        while (!isConnected)
+        {
+            Debug.Log("Trying to connect HAHMI server...");
+            try
+            {
+                webSocket.Connect();
+                Debug.Log("Connect is successfully");
+                isConnected = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
+
+            yield return new WaitForSecondsRealtime(periodTryingConnect);
+        }
+
+        yield return null;
+    }
+}
